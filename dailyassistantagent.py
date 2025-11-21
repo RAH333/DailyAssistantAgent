a782408b8f04c781e5ee2ad87ed4f3e54596a7fe
RAH333 <142141323+RAH333@users.noreply.github.com>
2025-11-21 22:39:42 +0530

    Update dailyassistantagent.py

--- dailyassistantagent.py
+++ dailyassistantagent.py
@@ -1,1 +1,52 @@
+# @title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+import os
+from kaggle_secrets import UserSecretsClient
+
+try:
+    GOOGLE_API_KEY = UserSecretsClient().get_secret("GOOGLE_API_KEY")
+    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    print("✅ Gemini API key setup complete.")
+except Exception as e:
+    print(
+        f"🔑 Authentication Error: Please make sure you have added 'GOOGLE_API_KEY' to your Kaggle secrets. Details: {e}"
+    )
+
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+from google.genai import types
+from google.adk.agents import Agent, SequentialAgent, ParallelAgent, LoopAgent
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.adk.agents import LlmAgent
+from google.adk.sessions import InMemorySessionService
+from google.adk.tools import google_search, AgentTool, ToolContext
+from google.adk.code_executors import BuiltInCodeExecutor
+
+print("✅ ADK components imported successfully.")
+
+
+
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
+)
+
+
+
 print(Hi, I am your Daily life Assistance.)
