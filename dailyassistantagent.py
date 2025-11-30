c0dd568b75f28ce9e9477a35115dde7ff2694c98
RAH333 <142141323+RAH333@users.noreply.github.com>
2025-11-30 11:23:15 +0530

    Update dailyassistantagent.py

--- dailyassistantagent.py
+++ dailyassistantagent.py
@@ -113,6 +113,14 @@ retry_config=types.HttpRetryOptions(
     initial_delay=1,
     http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
 )
+from google.adk.agents.llm_agent import Agent
+
+root_agent = Agent(
+    model='gemini-2.5-flash-lite',
+    name='root_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge',
+)
 print(Hi, I am your Daily life Assistant.)
 
 # Assistant Agent: Its job is to utilize various tools, agents, ADK, and models, such as the google_search tool, etc. Maintain and manage daily life data records, and process and present the records.
