60f29169fa3d252a88ee9f2b54e0851f907fbd56
RAH333 <142141323+RAH333@users.noreply.github.com>
2025-12-12 08:49:28 +0530

    Refactor agent setup and import statements

--- daily-assistant-agent-model/agents.py
+++ daily-assistant-agent-model/agents.py
@@ -7,15 +7,15 @@ from google.adk.tools import google_search
 from google.genai import types
 import os
 
-try:
-    #GOOGLE_API_KEY = UserSecretsClient().get_secret("GOOGLE_API_KEY")
-    GOOGLE_API_KEY = ""
-    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
-    print(" Gemini API key setup complete.")
-except Exception as e:
-    print(
-        f"🔑 Authentication Error: Please make sure you have added 'GOOGLE_API_KEY' to your Kaggle secrets. Details: {e}"
-    )
+##try:
+    ##GOOGLE_API_KEY = UserSecretsClient().get_secret("GOOGLE_API_KEY")
+    ##GOOGLE_API_KEY = ""
+    ##os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    ##print(" Gemini API key setup complete.")
+##except Exception as e:
+    ##print(
+        ##f"🔑 Authentication Error: Please make sure you have added 'GOOGLE_API_KEY' to your Kaggle secrets. Details: {e}"
+    ##)
 
 # Define helper functions that will be reused throughout the notebook
 
@@ -58,4 +58,5 @@ runner = InMemoryRunner(agent=root_agent)
 
 print("Runner created.")
 response =await runner.run_debug("What is Agent Development Kit from Google? What languages is the SDK available in?")
+
 
